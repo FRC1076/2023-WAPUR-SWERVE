039e92232776fb49cb117c8a648b9e22c742e80a
Cryplo <dylanliaa@gmail.com>
2023-11-19 15:53:32 -0500

    Initial Commit

---
+++ robot.py
@@ -0,0 +1,113 @@
+import math
+import time
+import sys
+import os
+from datetime import datetime
+
+import wpilib
+import wpilib.drive
+import wpimath.controller
+from wpilib import interfaces
+import rev
+import ctre
+from navx import AHRS
+from networktables import NetworkTables
+
+from robotconfig import robotconfig, MODULE_NAMES
+from controller import Controller
+"""
+from swervedrive import SwerveDrive
+from swervemodule import SwerveModule
+from swervemodule import ModuleConfig
+
+from swervedrive import BalanceConfig
+from swervedrive import TargetConfig
+from swervedrive import BearingConfig
+from swervedrive import VisionDriveConfig
+
+from swervometer import FieldConfig
+from swervometer import RobotPropertyConfig
+from swervometer import Swervometer
+
+from elevator import Elevator
+from grabber import Grabber
+from vision import Vision
+from logger import Logger
+from dashboard import Dashboard
+
+from tester import Tester
+"""
+
+class MyRobot(wpilib.TimedRobot):
+
+    def robotInit(self):
+        controllers = self.initControllers(robotconfig["CONTROLLERS"])
+        self.driver = controllers[0]
+        self.operator = controllers[1]
+        return
+    
+    def initLogger(self, dir):
+        return #Logger.getLogger(dir)
+    
+    def initControllers(self, config):
+        ctrls = []
+        self.log(config)
+        for ctrlConfig in config.values():
+            self.log(ctrlConfig)
+            ctrlID = ctrlConfig['ID']
+            ctrl = wpilib.XboxController(ctrlID)
+            dz = ctrlConfig['DEADZONE']
+            lta = ctrlConfig['LEFT_TRIGGER_AXIS']
+            rta = ctrlConfig['RIGHT_TRIGGER_AXIS']
+            ctrls.append(Controller(ctrl, dz, lta, rta))
+        return ctrls
+    
+    def initVision(self, config):
+        return
+    
+    def initElevator(self, config):
+        return
+    
+    def initGrabber(self, config):
+        return
+    
+    def initDrivetrain(self, config):
+        return
+    
+    def initAuton(self, config):
+        return
+    
+    def initTeleop(self):
+        return
+
+    def robotPeriodic(self):
+        return True
+    
+    def teleopPeriodic(self):
+        return
+    
+    def teleopDrivetrain(self):
+        return
+    
+    def teleopElevatorGrabber(self):
+        return
+    
+    def autonomousInit(self): #this or initAuton?
+        return
+    
+    def autonomousPeriodic(self):
+        return
+    
+    def teleopManeuver(self):
+        return
+    
+    def deadzoneCorrection(self, val, deadzone):
+        return
+
+    def log(self, *dataToLog):
+        return
+
+if __name__ == "__main__":
+    if sys.argv[1] == 'sim':
+        TEST_MODE = True
+    wpilib.run(MyRobot)
---
+++ robotconfig.py
@@ -0,0 +1,62 @@
+from collections import namedtuple
+
+DEADZONE = 0.1
+
+# Drive Types
+ARCADE = 1
+TANK = 2
+SWERVE = 3
+
+controllerConfig = {
+    "DRIVER": {
+        "ID": 0,
+        "DEADZONE": DEADZONE,
+        "LEFT_TRIGGER_AXIS": 2,
+        "RIGHT_TRIGGER_AXIS": 3,
+    },
+    "OPERATOR": {
+        "ID": 1,
+        "DEADZONE": DEADZONE,
+        "LEFT_TRIGGER_AXIS": 2,
+        "RIGHT_TRIGGER_AXIS": 3,
+    }
+}
+
+swervometerConfig = {
+
+}
+
+drivetrainConfig = {
+
+}
+
+visionConfig = {
+
+}
+
+elevatorConfig = {
+
+}
+
+grabberConfig = {
+
+}
+
+autonConfig = {
+
+}
+
+MODULE_NAMES = namedtuple('MODULE_NAMES', [
+])
+
+loggingConfig = {
+
+}
+
+dashboardConfig = {
+
+}
+
+robotconfig = {
+    "CONTROLLERS": controllerConfig,
+}
